6d2d67e917ee61de29f6fc4cc8f16887b36d2d89
Lucas Pereira <luquinhabat@gmail.com>
2021-01-27 12:40:38 -0300

    repetições

--- CursoJS/aula13/ambiente.js
+++ CursoJS/aula13/ambiente.js
@@ -1,8 +1,16 @@
 var c = 1
-while (c < 6) {
-    console.log('Tudo bem?')
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 15)
+
+/*
+var c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
     c++
 }
+*/
 
 /*
 console.log('Tudo bem?')
