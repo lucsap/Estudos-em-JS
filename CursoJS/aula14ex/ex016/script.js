262717ddf81abddefca10eb9cbca4866aadd927b
Lucas Pereira <luquinhabat@gmail.com>
2021-01-28 14:09:14 -0300

    exercício

---
+++ CursoJS/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+
+var inicio = document.getElementById(`txtstart`)
+var fim = document.getElementById('txtend')
+var passo = document.getElementById(`txtstep`)
+var res = document.querySelector('div#res')
+
+function contar() {
+    res.innerHTML = `${inicio.value} ${fim.value} ${passo.value}`
+    for() {
+        
+    }
+}
