722fb93484c79f877154f3a8dfe64c615acab3ce
Lucas Pereira <luquinhabat@gmail.com>
2021-02-02 10:20:58 -0300

    ex16 e 17

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -1,12 +1,35 @@
-
-var inicio = document.getElementById(`txtstart`)
-var fim = document.getElementById('txtend')
-var passo = document.getElementById(`txtstep`)
-var res = document.querySelector('div#res')
-
 function contar() {
-    res.innerHTML = `${inicio.value} ${fim.value} ${passo.value}`
-    for() {
-        
+    var inicio = document.getElementById(`txtstart`)
+    var fim = document.getElementById('txtend')
+    var passo = document.getElementById(`txtstep`)
+    var res = document.querySelector('div#res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        // window.alert(`[ERRO] Faltam dados!`)
+    } else {
+        res.innerHTML = `Contando: <br>`
+        // Fazer os valores digitados serem tratados como númemros
+        var start = Number(inicio.value)
+        var end = Number(fim.value)
+        var step = Number(passo.value)
+        if(step == 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            step = 1
+        }
+        if(start < end) {
+            // Contagem crescente
+            for(var c = start; c <= end; c += step) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else if(start > end) {
+            // Contagem decrescente
+            for(var c = start; c >= end; c-= step) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            window.alert('[ERRO] Por favor digite dados válidos!')
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -1,1 +1,31 @@
-alert(`Olá`)
+function tabuada() {
+    let num = document.getElementById('txtnumber')
+    let res = document.getElementById('res')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = '' // Limpa a área do SELECT antes de mostrar uma nova tabuada
+        for(let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}` // Útil para outras linguagens como PHP para saber qual o VALUE do item
+            tab.appendChild(item)
+        }
+    }
+}
+
+/*
+
+CÓDIGO DE TABUADA PARA RODAR NO TERMINAL
+
+let num = 4
+let res = 0
+
+for(let c = 0; c <= 10; c++) {
+    res = (num * c)
+    console.log(`${num} x ${c} = ${res}`)
+}
+*/
